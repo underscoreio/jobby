5f8dfc1514df71a5fd0abd3100fb18692a2c31f6
Richard Dallaway <richard@dallaway.com>
2016-08-19 15:07:47 +0100

    tabs? no way

--- src/main/scala/io/underscore/jobby/io.scala
+++ src/main/scala/io/underscore/jobby/io.scala
@@ -9,23 +9,23 @@ import scala.util.Try
 object IO {
 
   val dateFormat = DateTimeFormatter
-		.ofPattern("yyyy-MM-dd")
-		.withZone(ZoneId of "UTC")
+    .ofPattern("yyyy-MM-dd")
+    .withZone(ZoneId of "UTC")
 
   def filename(job: Job, path: String = "target"): Path = {
-		val name = s"${dateFormat.format(job.timestamp)}-${slug(job)}.md"
-		FileSystems.getDefault().getPath(path, name)
-	}
+    val name = s"${dateFormat.format(job.timestamp)}-${slug(job)}.md"
+    FileSystems.getDefault().getPath(path, name)
+  }
 
   def slug(job: Job): String =
     job.companyName.toLowerCase.replaceAll("\\W","-")
 
   def write(job: Job): Try[Path] = Try {
-		val path = filename(job)
-		val content = Markdown(job)
-		val w = Files.newBufferedWriter(path, Charset.forName("UTF-8"))
-		try w.write(content) finally w.close
-		path
+    val path = filename(job)
+    val content = Markdown(job)
+    val w = Files.newBufferedWriter(path, Charset.forName("UTF-8"))
+    try w.write(content) finally w.close
+    path
   }
-  
+
 }
